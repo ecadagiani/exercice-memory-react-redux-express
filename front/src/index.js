00dc0212647c1a26bda6dfd1831fdaddc7fd19a1
Eden <e.cadagiani@gmail.com>
2021-02-20 21:17:53 +0100

    feat: use front react

--- front/src/index.js
+++ front/src/index.js
@@ -1,16 +1,17 @@
-/*
- * Created in février 2021
- * Written by Eden Cadagiani <e.cadagiani@gmail.com>
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import './index.scss'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const change = msg => {
-    document.querySelector('body').innerText = msg
-}
-
-document.querySelector('body').innerText = 'Hello, World!'
-
-setTimeout(() => {
-    change('Deferred hello world!')
-}, 3000)
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
